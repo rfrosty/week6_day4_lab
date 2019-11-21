e3c3f5ae25f15d43150c57d7b79e8690749d9dcb
L3n1ad <danieltoth0910@gmail.com>
2019-11-21 16:11:51 +0000

    finished mvp

--- lab_events_start/js/app.js
+++ lab_events_start/js/app.js
@@ -1,3 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript loaded');
-})
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit);
+});
+
+const handleFormSubmit = function(event){
+  event.preventDefault();
+  console.log(event);
+  // create li for title
+  const listItemTitle = document.createElement('li');
+  listItemTitle.textContent = event.target.title.value;
+  // create li for author
+  const listItemAuthor = document.createElement('li');
+  listItemAuthor.textContent = event.target.author.value;
+
+  const listItemCategory = document.createElement('li');
+  listItemCategory.textContent = event.target.category.value;
+
+  const list = document.querySelector('#reading-list');
+  list.appendChild(listItemTitle);
+  list.appendChild(listItemAuthor);
+  list.appendChild(listItemCategory);
+
+  document.querySelector('#new-item-form').reset()
+};
